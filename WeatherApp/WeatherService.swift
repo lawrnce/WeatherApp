d95472069cea29346effa2ab0c714b8a3ad5213c
Lawrence Tran <lawrence@tran.com>
2016-04-23 21:23:26 -0500

    Test success

--- WeatherApp/WeatherService.swift
+++ WeatherApp/WeatherService.swift
@@ -13,7 +13,23 @@ let kFORECAST_API_KEY = "5978bff4ccea26c30b3b97b0e818e369"
 let kFORECAST_API_URL = "https://api.forecast.io/forecast/"
 
 class WeatherService {
-    class func getWeatherDataForCoordinate(coordinate: CLLocationCoordinate2D, completion:(data: String, error: ErrorType) -> Void) {
-        print("From Service ", coordinate)
+    
+    /**
+     
+    
+    */
+    class func getWeatherDataForCoordinate(coordinate: CLLocationCoordinate2D, completion:(data: String, error: ErrorType?) -> Void) {
+        let apiCall = kFORECAST_API_URL + kFORECAST_API_KEY + "/\(coordinate.latitude),\(coordinate.longitude)"
+        Alamofire.request(.GET, apiCall)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                    completion(data: "test", error: nil)
+                }
+            }
     }
 }
